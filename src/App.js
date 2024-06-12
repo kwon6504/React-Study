92a11a655256a529ecb7654aea9440fc2f6c497f
DESKTOP-5UVG7U6\kwon <gurdud0328@naver.com>
2024-06-12 12:18:50 +0900

    Commponent 사용법

--- src/App.js
+++ src/App.js
@@ -35,16 +35,19 @@ function App() {
         <h4 style={{ color: 'red', fontSize: '16px' }}>블로그임</h4>
       </div>
 
+      {/* 내가 작성한 숙제 답변 sort = 정렬함수, localeCompare = 문자열 비교함수 */}
       <button onClick={() => {
         let sortkr = [...글제목].sort((a, b) => a.localeCompare(b, 'ko-KR'));
         글제목변경(sortkr);
       }}>가나다순정렬</button>
 
-      {/* <button onClick={() => {
+      {/* 코딩애플 답변
+       <button onClick={() => {
         let copy = [...글제목];
         copy.sort();
         글제목변경(copy)
       }}> 정렬버튼 </button> */}
+
 
       <button onClick={() => { //기존거state == 신규state의 경우 변경안해줌
         let copy = [...글제목]; //...괄호를 나오게 한후 변경해달라는
@@ -66,8 +69,43 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      <Modal></Modal>
+
     </div>
   );
 }
+
+
+{/* 컴포넌트(Component) 만드는법
+      1. function만들고
+      2. return() 안에 html 담기
+      3. <함수명></함수명>쓰기
+      컴포넌트 사용하기좋을때
+      1. 반복적인 html 축약할 때
+      2. 큰 페이지들
+      3. 자주변경되는 것들
+    컴포넌트 단점 : state 가져다쓸 때 문제생김(A함수에 잇던 변수는 B함수에서 맘대로 가져다 쓸 수 없음)*/}
+function Modal() {
+  return (
+    //의미 없는 div 만들기 싫을때 <></>사용가능하다 
+    <>
+      <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+    </>
+  );
+}
+
+// (참고) 컴포넌트 만드는 문법 2
+// let 보다 const를 많이 사용한다.
+// let Modal = () => {
+//   return(
+//     <div>
+//     </div>
+//   );
+// };
 
 export default App;
